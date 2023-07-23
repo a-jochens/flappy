6da8c764cf985d8754ba7793f402bc8090811bed
Arne Jochens <A.Jochens@yahoo.com>
2023-07-23 22:15:01 +0200

    Bracket-lib setup.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Flappy Dragon
+
+Based on Herbert Wolverson, *Hands-on Rust*, chapter 3.
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 #![warn(clippy::all, clippy::pedantic)]
-# Flappy Dragon
+
+// Flappy Dragon
+
+use bracket_lib::prelude::*;
 
 
+struct State {}
 
-fn main() {
-    println!("Hello, world!");
+// trait definition
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State{})
 }
