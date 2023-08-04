07af30278b9c01e8aef68fa16c5f16369312d11c
Arne Jochens <A.Jochens@yahoo.com>
2023-08-04 21:52:19 +0200

    Implemented player and gravity.

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,86 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 75.0;
+
+struct Player {
+    x: i32,  // Position in horizontal world space.
+    y: i32,  // Position in vertical screen space.
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(  // bracket-lib function to set a single character on the screen.
+            0,  // Dragon is always shown on the left end of the screen.
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')  // Unicode to codepage 437.
+        );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: Fill in this stub later.
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);  // Clear screen and color the background.
+        self.frame_time += ctx.frame_time_ms;  // Increment by elapsed time.
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {  // Dropped off the bottom.
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
